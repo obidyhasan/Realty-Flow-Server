91c3ee13adf77010da090ae1af3410df9e02db9f
obidyhasan <obidyhasan@gmail.com>
2025-01-19 14:23:11 +0600

    include vercel.json

--- index.js
+++ index.js
@@ -610,9 +610,9 @@ async function run() {
       res.send(result);
     });
 
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
